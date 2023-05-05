da724f4bd06eac7c3e7579cdfae2a3bfeeb395dc
ErtugrulSener <ertugrulsener@hotmail.de>
2023-05-05 18:07:57 +0000

    feat: add config file fow flowershow project

---
+++ content/config.mjs
@@ -0,0 +1,9 @@
+const config = {
+  title: "SecondBrain",
+  description: "This is my SecondBrain.",
+  author: "Ertugrul",
+  authorUrl: "https://ertugrulsener.de/",
+  contentExclude: ["Kanbanboard.md"]
+};
+
+export default config;
