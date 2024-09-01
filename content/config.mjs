c9f9f4e25ca0dc92bb4e4cbda3f5d1b75e79b415
ErtugrulSener <ertugrulsener@hotmail.de>
2024-09-01 10:56:59 +0200

    vault backup: 2024-09-01 10:56:59

--- content/config.mjs
+++
@@ -1,9 +0,0 @@
-const config = {
-  title: "SecondBrain",
-  description: "This is my SecondBrain.",
-  author: "Ertugrul",
-  authorUrl: "https://ertugrulsener.de/",
-  contentExclude: ["Kanbanboard.md"]
-};
-
-export default config;
